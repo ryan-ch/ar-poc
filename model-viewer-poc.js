df93a37cd6abe8e1b4814e3d5b6fbf03dc6f86b4
ryan-ch <47079036+ryan-ch@users.noreply.github.com>
2022-07-19 16:57:41 +0800

    Updates

---
+++ model-viewer-poc.js
@@ -0,0 +1,255 @@
+/* global AFRAME, THREE */
+AFRAME.registerComponent('model-viewer', {
+    schema: {
+      gltfModel: {default: ''}
+    },
+    init: function () {
+      this.onModelLoaded = this.onModelLoaded.bind(this);
+      this.onMouseUp = this.onMouseUp.bind(this);
+      this.onMouseMove = this.onMouseMove.bind(this);
+      this.onMouseDown = this.onMouseDown.bind(this);
+      this.onMouseWheel = this.onMouseWheel.bind(this);
+      this.onTouchMove = this.onTouchMove.bind(this);
+      this.onTouchEnd = this.onTouchEnd.bind(this);
+      this.onOrientationChange = this.onOrientationChange.bind(this);
+  
+      this.initCameraRig();
+      this.initEntities();
+  
+      // Disable context menu on canvas when pressing mouse right button;
+      this.el.sceneEl.canvas.oncontextmenu = function (evt) { evt.preventDefault(); };
+  
+      window.addEventListener('orientationchange', this.onOrientationChange);
+  
+      // Mouse 2D controls.
+      document.addEventListener('mouseup', this.onMouseUp);
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mousedown', this.onMouseDown);
+      document.addEventListener('wheel', this.onMouseWheel);
+  
+      // Mobile 2D controls.
+      document.addEventListener('touchend', this.onTouchEnd);
+      document.addEventListener('touchmove', this.onTouchMove);
+  
+      this.modelEl.addEventListener('model-loaded', this.onModelLoaded);
+    },
+
+    update: function () {
+        if (!this.data.gltfModel) { return; }
+        this.modelEl.setAttribute('gltf-model', this.data.gltfModel);
+      },
+  
+    initCameraRig: function () {
+      var cameraRigEl = this.cameraRigEl = document.createElement('a-entity');
+      var cameraEl = this.cameraEl = document.createElement('a-entity');
+  
+      cameraEl.setAttribute('camera', {fov: 60});
+      cameraEl.setAttribute('look-controls', {
+        magicWindowTrackingEnabled: false,
+        mouseEnabled: false,
+        touchEnabled: false
+      });
+  
+      cameraRigEl.appendChild(cameraEl);
+      this.el.appendChild(cameraRigEl);
+    },
+  
+    initEntities: function () {
+      var containerEl = this.containerEl = document.createElement('a-entity');
+      var modelPivotEl = this.modelPivotEl = document.createElement('a-entity');
+      // This is our glTF model entity.
+      var modelEl = this.modelEl = document.createElement('a-entity');
+  
+      modelPivotEl.id = 'modelPivot';
+      modelEl.id = 'model';  
+      modelEl.setAttribute('rotation', '0 -30 0');
+      modelEl.setAttribute('animation-mixer', '');
+      modelPivotEl.appendChild(modelEl);
+  
+      this.containerEl.appendChild(modelPivotEl);
+      this.el.appendChild(containerEl);
+    },
+  
+    onMouseWheel: function (evt) {
+      var modelPivotEl = this.modelPivotEl;
+      var modelScale = this.modelScale || modelPivotEl.object3D.scale.x;
+      modelScale -= evt.deltaY / 100;
+      modelScale = Math.min(Math.max(0.8, modelScale), 2.0);
+      // Clamp scale.
+      modelPivotEl.object3D.scale.set(modelScale, modelScale, modelScale);
+      this.modelScale = modelScale;
+    },
+  
+    onOrientationChange: function () {
+      if (AFRAME.utils.device.isLandscape()) {
+        this.cameraRigEl.object3D.position.z -= 1;
+      } else {
+        this.cameraRigEl.object3D.position.z += 1;
+      }
+    },
+  
+    onTouchMove: function (evt) {
+      this.oldClientX = this.oldClientX || evt.touches[0].clientX;
+      this.oldClientY = this.oldClientY || evt.touches[0].clientY;
+      if (evt.touches.length === 1) { this.onSingleTouchMove(evt); }
+      if (evt.touches.length === 2) { this.onPinchMove(evt); }
+    },
+  
+    onSingleTouchMove: function (evt) {
+      var dX;
+      var dY;
+      var modelPivotEl = this.modelPivotEl;
+      this.oldClientX = this.oldClientX || evt.touches[0].clientX;
+      this.oldClientY = this.oldClientY || evt.touches[0].clientY;
+  
+      dX = this.oldClientX - evt.touches[0].clientX;
+      dY = this.oldClientY - evt.touches[0].clientY;
+  
+      modelPivotEl.object3D.rotation.y -= dX / 200;
+      this.oldClientX = evt.touches[0].clientX;
+  
+      modelPivotEl.object3D.rotation.x -= dY / 100;
+  
+      // Clamp x rotation to [-90,90]
+      modelPivotEl.object3D.rotation.x = Math.min(Math.max(-Math.PI / 2, modelPivotEl.object3D.rotation.x), Math.PI / 2);
+      this.oldClientY = evt.touches[0].clientY;
+    },
+  
+    onPinchMove: function (evt) {
+      var dX = evt.touches[0].clientX - evt.touches[1].clientX;
+      var dY = evt.touches[0].clientY - evt.touches[1].clientY;
+      var modelPivotEl = this.modelPivotEl;
+      var distance = Math.sqrt(dX * dX + dY * dY);
+      var oldDistance = this.oldDistance || distance;
+      var distanceDifference = oldDistance - distance;
+
+      if (Math.abs(dX) < 100 && Math.abs(dY) < 100) {
+        this.dragModel(evt.touches[0]);
+      }
+      else {
+        var modelScale = this.modelScale || modelPivotEl.object3D.scale.x;
+  
+        modelScale -= distanceDifference / 500;
+        modelScale = Math.min(Math.max(0.8, modelScale), 2.0);
+        // Clamp scale.
+        modelPivotEl.object3D.scale.set(modelScale, modelScale, modelScale);
+      }
+
+      this.modelScale = modelScale;
+      this.oldDistance = distance;
+    },
+  
+    onTouchEnd: function (evt) {
+      this.oldClientX = undefined;
+      this.oldClientY = undefined;
+      if (evt.touches.length < 2) { this.oldDistance = undefined; }
+    },
+  
+    onMouseUp: function (evt) {
+      this.leftRightButtonPressed = false;
+      if (evt.buttons === undefined || evt.buttons !== 0) { return; }
+      this.oldClientX = undefined;
+      this.oldClientY = undefined;
+    },
+  
+    onMouseMove: function (evt) {
+      if (this.leftRightButtonPressed) {
+        this.dragModel(evt);
+      } else {
+        this.rotateModel(evt);
+      }
+    },
+  
+    dragModel: function (evt) {
+      var dX;
+      var dY;
+      var modelPivotEl = this.modelPivotEl;
+      if (!this.oldClientX) { return; }
+      dX = this.oldClientX - evt.clientX;
+      dY = this.oldClientY - evt.clientY;
+      modelPivotEl.object3D.position.y += dY / 200;
+      modelPivotEl.object3D.position.x -= dX / 200;
+      this.oldClientX = evt.clientX;
+      this.oldClientY = evt.clientY;
+    },
+  
+    rotateModel: function (evt) {
+      var dX;
+      var dY;
+      var modelPivotEl = this.modelPivotEl;
+      if (!this.oldClientX) { return; }
+      dX = this.oldClientX - evt.clientX;
+      dY = this.oldClientY - evt.clientY;
+      modelPivotEl.object3D.rotation.y -= dX / 100;
+      modelPivotEl.object3D.rotation.x -= dY / 200;
+  
+      // Clamp x rotation to [-90,90]
+      modelPivotEl.object3D.rotation.x = Math.min(Math.max(-Math.PI / 2, modelPivotEl.object3D.rotation.x), Math.PI / 2);
+  
+      this.oldClientX = evt.clientX;
+      this.oldClientY = evt.clientY;
+    },
+  
+    onModelLoaded: function () {
+      this.centerAndScaleModel();
+    },
+  
+    centerAndScaleModel: function () {
+      var box;
+      var size;
+      var center;
+      var scale;
+      var modelEl = this.modelEl;
+      var shadowEl = this.shadowEl;
+      var titleEl = this.titleEl;
+      var gltfObject = modelEl.getObject3D('mesh');
+  
+      // Reset position and scales.
+      modelEl.object3D.position.set(0, -1.0, 0);
+      modelEl.object3D.scale.set(1.0, 1.0, 1.0);
+      this.cameraRigEl.object3D.position.z = 3.0;
+  
+      // Calculate model size.
+      modelEl.object3D.updateMatrixWorld();
+      box = new THREE.Box3().setFromObject(gltfObject);
+      size = box.getSize(new THREE.Vector3());
+  
+      // Calculate scale factor to resize model to human scale.
+      scale = 1.6 / size.y;
+      scale = 2.0 / size.x < scale ? 2.0 / size.x : scale;
+      scale = 2.0 / size.z < scale ? 2.0 / size.z : scale;
+  
+      modelEl.object3D.scale.set(scale, scale, scale);
+  
+      // Center model at (0, 0, 0).
+      modelEl.object3D.updateMatrixWorld();
+      box = new THREE.Box3().setFromObject(gltfObject);
+      center = box.getCenter(new THREE.Vector3());
+      size = box.getSize(new THREE.Vector3());
+  
+      shadowEl.object3D.scale.y = size.x;
+      shadowEl.object3D.scale.x = size.y;
+      shadowEl.object3D.position.y = -size.y / 2;
+      shadowEl.object3D.position.z = -center.z;
+      shadowEl.object3D.position.x = -center.x;
+  
+      titleEl.object3D.position.x = 2.2 - center.x;
+      titleEl.object3D.position.y = size.y + 0.5;
+      titleEl.object3D.position.z = -2;
+      titleEl.object3D.visible = true;
+  
+      modelEl.object3D.position.x = -center.x;
+      modelEl.object3D.position.y = -center.y;
+      modelEl.object3D.position.z = -center.z;
+  
+      // When in mobile landscape we want to bring the model a bit closer.
+      if (AFRAME.utils.device.isLandscape()) { this.cameraRigEl.object3D.position.z -= 1; }
+    },
+  
+    onMouseDown: function (evt) {
+      if (evt.buttons) { this.leftRightButtonPressed = evt.buttons === 3; }
+      this.oldClientX = evt.clientX;
+      this.oldClientY = evt.clientY;
+    }
+  });
+  
